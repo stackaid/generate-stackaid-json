4e72843c72c12a67aa0147c9916f8890e76d2bd9
Wes Carr <wescarr@gmail.com>
2022-09-06 13:11:46 -0700

    Fix discovery of Go direct dependencies (#6)
    * Fix discovery of direct dependencies
    
    - Our current approach relied on a main.go file residing at the root of the repository which may not always be the case.
    - Additionally calling go list {{.Deps}} module would not always give indented results.
    
    * Bump version

--- src/go.ts
+++ src/go.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import path from 'path'
 import { GITHUB_DOMAIN } from './constants'
 import { execSync } from 'child_process'
-import { groupBy, uniqBy } from 'lodash'
+import { uniqBy } from 'lodash'
 
 const sourceDir = core.getInput('src_dir') || process.cwd()
 
@@ -20,33 +20,77 @@ const parseDependency = (line: string) => {
   }
 }
 
-export const listDeps = (dir: string, module: string = '') => {
-  let output = execSync(`go list -f {{.Deps}} ${module}`, {
+const parseModuleUrl = (m: string) => {
+  const [url, version = ''] = m.split('@')
+  const [domain, owner, repo] = url.split('/')
+
+  return { module: [domain, owner, repo].join('/'), version }
+}
+
+export const listDirectDeps = (dir: string) => {
+  let output = execSync(
+    `go list -f '{{if not .Indirect}}{{.}}{{end}}' -m all`,
+    { cwd: resolveDir(dir) }
+  ).toString()
+
+  return output
+    .split('\n')
+    .map((d) => {
+      const [module, version = ''] = d.split(' ')
+      return { module, version }
+    })
+    .filter((entry) => filterDependency(entry.module))
+}
+
+export const getModuleGraph = (dir: string) => {
+  const output = execSync(`go mod graph`, {
     cwd: resolveDir(dir),
   }).toString()
-  // trim `[]` at start and end of string
-  output = output.slice(1, -1)
 
-  return output.split(/\s+/).filter(filterDependency)
+  const graph: Record<string, { module: string; version: string }[]> = {}
+
+  output.split('\n').forEach((line) => {
+    if (!line) {
+      return
+    }
+
+    const [parent, child] = line.split(' ')
+    const mod = parseModuleUrl(parent)
+    const childMod = parseModuleUrl(child)
+
+    const key = `${mod.module}@${mod.version}`
+    graph[key] = graph[key] || []
+
+    if (childMod.module !== key) {
+      graph[key].push(childMod)
+    }
+  })
+
+  Object.entries(graph).forEach(([key, deps]) => {
+    graph[key] = uniqBy(deps, 'module')
+  })
+
+  return graph
 }
 
 export const getDependencies = (dir: string = '') => {
-  const direct = listDeps(dir)
-  let dependencies = direct.map((d) => ({
-    source: parseDependency(d),
-    dependencies: listDeps(dir, d).map((d) => ({ source: parseDependency(d) })),
-  }))
+  const graph = getModuleGraph(dir)
+  const direct = listDirectDeps(dir)
 
-  // Merge direct dependencies with the same source
-  // and remove self dependencies
-  const groups = groupBy(dependencies, 'source')
-  return Object.entries(groups).map(([source, group]) => {
-    return {
-      source,
-      dependencies: uniqBy(
-        group.flatMap((g) => g.dependencies),
-        'source'
-      ).filter((d) => d.source !== source),
-    }
-  }) as StackAidDependency[]
+  let dependencies = direct
+    .filter((d) => filterDependency(d.module))
+    .map((d) => {
+      const url = parseModuleUrl(d.module).module
+      const deps = graph[`${url}@${d.version}`] || []
+      return {
+        source: parseDependency(d.module) as string,
+        dependencies: deps
+          .filter((d) => filterDependency(d.module))
+          .map((d) => ({
+            source: parseDependency(d.module) as string,
+          })),
+      }
+    })
+
+  return dependencies as StackAidDependency[]
 }
